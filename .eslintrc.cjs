c8e78aed40f51626535d1af7f4b97efb8f682edb
behnam rahimpour <74137947+behnamrhp@users.noreply.github.com>
2022-10-09 15:48:07 +0330

    [FEAT]: husky and eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,36 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:prettier/recommended",
+        "plugin:jest/recommended"
+    ],
+    "overrides": [
+    ],
+    "settings": {
+        "react": {
+            "version": "detect"
+        }
+    },
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "@typescript-eslint",
+        "jest"
+    ],
+    "rules": {
+        "eqeqeq": "error",
+        "prettier/prettier": "off",
+        "react/react-in-jsx-scope": "off",
+        "no-unused-vars": "error"
+    }
+}
---
+++ src/__test__/app.test.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { fireEvent, render, screen } from '@testing-library/react'
+import App from "../App"
+
+test("click increase button", () => {
+    const appEl = render(<App />)
+
+    const IncreaseButton = appEl.getByTestId('increasing-btn');
+
+    fireEvent.click(IncreaseButton,{})
+
+    const increasedNumber = screen.getByTestId('increasing-btn').textContent;
+
+    expect(increasedNumber).toBe('count is 2')
+})
