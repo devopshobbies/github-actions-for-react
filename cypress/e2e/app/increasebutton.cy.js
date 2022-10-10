a308bad21f0cea2c1fb6827db7a126f3689b5a97
behnam rahimpour <74137947+behnamrhp@users.noreply.github.com>
2022-10-10 10:57:24 +0330

    [FEAT]: add cypress simple test

---
+++ cypress/e2e/app/increasebutton.cy.js
@@ -0,0 +1,15 @@
+
+describe('basic ui', () => {
+    beforeEach( () => {
+        cy.visit('http://localhost:3030')
+        cy.get('[data-testid="increasing-btn"').as('incBtn')
+
+
+    })
+    it('onClick increase button', function() {
+
+        cy.get('@incBtn').click()
+        
+        cy.get('@incBtn').should('have.text', "count is 1")
+    })
+})
