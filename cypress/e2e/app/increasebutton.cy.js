55bf0d7166aabac59f1971e6c8329e95474b4a02
behnam rahimpour <74137947+behnamrhp@users.noreply.github.com>
2022-10-16 16:33:13 +0330

    [DEBUG]: debug port project

--- cypress/e2e/app/increasebutton.cy.js
+++ cypress/e2e/app/increasebutton.cy.js
@@ -1,10 +1,8 @@
 
 describe('basic ui', () => {
     beforeEach( () => {
-        cy.visit(import.meta.env.VITE_DEV_PORT)
+        cy.visit('http://localhost:3000')
         cy.get('[data-testid="increasing-btn"').as('incBtn')
-
-
     })
     it('onClick increase button', function() {
 
