5c017af401d2fa1cace5fc9d4e8639f54326baec
behnam rahimpour <74137947+behnamrhp@users.noreply.github.com>
2022-10-16 16:36:41 +0330

    [REF]: test not valid jest in CI

--- src/__test__/app.test.tsx
+++ src/__test__/app.test.tsx
@@ -11,5 +11,5 @@ test("click increase button", () => {
 
     const increasedNumber = screen.getByTestId('increasing-btn').textContent;
 
-    expect(increasedNumber).toBe('count is 1')
+    expect(increasedNumber).toBe('count is 2')
 })
